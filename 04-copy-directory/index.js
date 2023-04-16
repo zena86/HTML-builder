765b56e5ad97e690cdd5503285ce0e459cc6ce84
Yauheniya Mastavaya <jk@db.by>
2023-04-16 19:03:09 +0300

    feat: implement an update for copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,6 +7,16 @@ const copyFolderPath = path.join(__dirname, "files-copy");
 
 fsPromises.mkdir(copyFolderPath, { recursive: true }).catch((err) => console.log(err));
 
+fs.readdir(copyFolderPath, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(copyFolderPath, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
 fsPromises.readdir(sourceFolderPath, {withFileTypes: true}).then(filenames => {
   for (let filename of filenames) {
     let sourceFilePath = path.join(sourceFolderPath, filename.name);
