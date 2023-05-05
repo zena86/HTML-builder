486bf309d2a17d43babd92380855c1608704db98
Yauheniya Mastavaya <jk@db.by>
2023-05-05 21:12:44 +0300

    feat: implement 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,107 +3,157 @@
 let fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
+const process = require('process');
 
 const distFolderPath = path.join(__dirname, 'project-dist');
+const distAssetsFolderPath = path.join(distFolderPath, 'assets');
 
-let tmpHtml = '';
+async function hasAccessFunc(filePath) {
+  return new Promise(resolve => {
+    fs.access(filePath, fs.constants.F_OK, (error) => {
+      if (error) { resolve(false); }
 
-// Создать папку project-dist
-fs.mkdir(distFolderPath, { recursive: true }, err => {
-  if (err) throw err;
-  console.log('папка создана');
-});
+      fs.stat(filePath, (err) => {
+        if (err) { resolve(false); } else {
+          resolve(true);
+        }
+      });
+    });
+  });
+}
 
-// Копировать папку с изображениями
-const sourceAssetsPath = path.join(__dirname, 'assets');
-const copyAssetsPath = path.join(distFolderPath, 'assets');
+async function copyFileAsync(from, to) {
+  return new Promise(resolve => {
+    fs.copyFile(from, to, (err) => {
+      if (err) {
+        process.stdout.write(err);
+      }
+      resolve(err === null);
+    });
+  });
+}
 
-fsPromises.mkdir(copyAssetsPath, { recursive: true }).catch((err) => console.log(err));
+async function createFolder(folder) {
+  return new Promise(resolve => {
+    fs.mkdir(folder, { recursive: true }, (err) => {
+      if (err) {
+        process.stdout.write(err);
+      }
+      resolve(err == null);
+    });
+  });
+}
 
-// fs.readdir(copyAssetsPath, (err, files) => {
-//   if (err) throw err;
+async function build() {
+  let tmpHtml = '';
 
-//   for (const file of files) {
-//     fs.unlink(path.join(copyAssetsPath, file), (err) => {
-//       if (err) throw err;
-//     });
-//   }
-// });
+  // Создать папку project-dist
+  if (!(await hasAccessFunc(distAssetsFolderPath))) {
+    await fs.mkdir(distAssetsFolderPath, { recursive: true }, (err) => {
+      if (err) process.stdout.write(err);
+    });
+  }
 
-async function copyFilesTree(source, copy) {
-  await fsPromises.readdir(source, { withFileTypes: true }).then(files => {
-    files.forEach(async (file) => {
-      if (file.isFile()) {
-        let sourceFilePath = path.join(source, file.name);
-        let copyFilePath = path.join(copy, file.name);
-        fs.copyFile(sourceFilePath, copyFilePath, err => { if (err) throw err; });
-      } else if (file.isDirectory()) {
-        let sourcePath = path.join(source, file.name);
-        let copyPath = path.join(copy, file.name);
+  // Копировать папку с изображениями
+  const sourceAssetsPath = path.join(__dirname, 'assets');
+  const copyAssetsPath = path.join(distFolderPath, 'assets');
 
-        fs.mkdir(copyPath, { recursive: true }, err => {
-          if (err) throw err;
+  async function clearAssets(fromPath) {
+    const hasAccess = await hasAccessFunc(fromPath);
+    if (!hasAccess) { return; }
+
+    const paths = await fsPromises.readdir(fromPath, { withFileTypes: true });
+    paths.forEach(async innerPath => {
+      const fullPath = path.join(fromPath, innerPath.name);
+      if (innerPath.isFile()) {
+        await fs.unlink(fullPath, (error) => {
+          if (error) process.stdout.write(error);
         });
+      } else {
+        await clearAssets(fullPath);
+      }
+    });
+  }
 
+  await clearAssets(copyAssetsPath);
+
+  async function copyFilesTree(source, copy) {
+    const pathes = await fsPromises.readdir(source, { withFileTypes: true });
+    pathes.forEach(async (file) => {
+      let sourcePath = path.join(source, file.name);
+      let copyPath = path.join(copy, file.name);
+
+      if (file.isFile()) {
+        await copyFileAsync(sourcePath, copyPath);
+      } else if (file.isDirectory()) {
+        const isFolderExist = await hasAccessFunc(copyPath);
+        if (!isFolderExist) {
+          await createFolder(copyPath);
+        }
         await copyFilesTree(sourcePath, copyPath);
       }
     });
-  }).catch(err => console.log(err));
-}
+  }
 
-copyFilesTree(sourceAssetsPath, copyAssetsPath);
+  await copyFilesTree(sourceAssetsPath, copyAssetsPath);
 
-// Создать файл стилей
-(function mergeStyles() {
-  const bundleCssPath = path.join(__dirname, 'project-dist', 'style.css');
-  const writableStream = fs.createWriteStream(bundleCssPath);
-  const folderPath = path.join(__dirname, 'styles');
+  // Создать файл стилей
+  (function mergeStyles() {
+    const bundleCssPath = path.join(__dirname, 'project-dist', 'style.css');
+    const writableStream = fs.createWriteStream(bundleCssPath);
+    const folderPath = path.join(__dirname, 'styles');
 
-  fs.promises.readdir(folderPath, { withFileTypes: true }).then(filenames => {
-    for (let filename of filenames) {
-      let filePath = path.join(__dirname, 'styles', filename.name);
-
-      if (filename.isFile() && path.extname(filePath) === '.css') {
-        const readableStream = fs.createReadStream(filePath);
-        readableStream.on('data', (chunk) => {
-          writableStream.write(chunk.toString() + '\n');
+    fs.promises
+      .readdir(folderPath, { withFileTypes: true })
+      .then((filenames) => {
+        filenames.forEach((filename) => {
+          let filePath = path.join(__dirname, 'styles', filename.name);
+          if (filename.isFile() && path.extname(filePath) === '.css') {
+            const readableStream = fs.createReadStream(filePath);
+            readableStream.on('data', (chunk) => {
+              writableStream.write(chunk.toString() + '\n');
+            });
+          }
         });
-      }
-    }
-  }).catch(err => {
-    console.log(err);
-  });
-}());
-
-// Создать файл html
-function createHtmlBundle() {
-  const sourceHtmlPath = path.join(__dirname, 'template.html');
-  const readableHtmlStream = fs.createReadStream(sourceHtmlPath);
-  readableHtmlStream.on('data', (chunk) => {
-    tmpHtml = chunk.toString();
-    analizeComponents();
-  });
-}
-createHtmlBundle();
-
-function analizeComponents() {
-  const componentsPath = path.join(__dirname, 'components');
-  fs.promises.readdir(componentsPath, { withFileTypes: true }).then(files => {
-    for (let file of files) {
-      let filePath = path.join(__dirname, 'components', file.name);
-      let fileName = path.parse(filePath).name;
-
-      const readableComponentStream = fs.createReadStream(filePath);
-      readableComponentStream.on('data', (chunk) => {
-        tmpHtml = tmpHtml.replace(`{{${fileName}}}`, chunk.toString());
-
-        const htmlBundlePath = path.join(distFolderPath, 'index.html');
-        const writableStream = fs.createWriteStream(htmlBundlePath);
-        writableStream.write(tmpHtml);
-        writableStream.end();
+      })
+      .catch((err) => {
+        process.stdout.write(err);
       });
-    }
-  }).catch(err => {
-    console.log(err);
-  });
+  }());
+
+  // Создать файл html
+  function analizeComponents() {
+    const componentsPath = path.join(__dirname, 'components');
+    fs.promises
+      .readdir(componentsPath, { withFileTypes: true })
+      .then((files) => {
+        files.forEach((file) => {
+          let filePath = path.join(__dirname, 'components', file.name);
+          let fileName = path.parse(filePath).name;
+          const readableComponentStream = fs.createReadStream(filePath);
+          readableComponentStream.on('data', (chunk) => {
+            tmpHtml = tmpHtml.replace(`{{${fileName}}}`, chunk.toString());
+            const htmlBundlePath = path.join(distFolderPath, 'index.html');
+            const writableStream = fs.createWriteStream(htmlBundlePath);
+            writableStream.write(tmpHtml);
+            writableStream.end();
+          });
+        });
+      })
+      .catch((err) => {
+        process.stdout.write(err);
+      });
+  }
+
+  function createHtmlBundle() {
+    const sourceHtmlPath = path.join(__dirname, 'template.html');
+    const readableHtmlStream = fs.createReadStream(sourceHtmlPath);
+    readableHtmlStream.on('data', (chunk) => {
+      tmpHtml = chunk.toString();
+      analizeComponents();
+    });
+  }
+  createHtmlBundle();
 }
+
+build();
